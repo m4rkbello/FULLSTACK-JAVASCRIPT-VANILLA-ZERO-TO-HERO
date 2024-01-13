3e3ecd2544cd487c76dd4b63e5742df03c12878d
M4rk <89385349+m4rkbello@users.noreply.github.com>
2024-01-13 23:49:04 +0800

    if dili ako aws naa kay lain else

---
+++ 018 - If else if else condition/index.js
@@ -0,0 +1,28 @@
+
+
+//kuhaon ang data 
+var  data = prompt();
+
+//e compare ang data sa condition
+if(data === "MARK"){ // stricly equal 
+    console.log("YOUR NAME IS", data);
+}else if(data === "BOY"){
+    console.log("YOUR NAME IS", data);
+}else{
+    console.log("YOUR NOT THE PERSON!");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
