febad60dd28634e626581d8dafa0b2c60cc5b237
M4rk <89385349+m4rkbello@users.noreply.github.com>
2024-01-14 12:05:51 +0800

    WHILE LOOP, DO WHILE, FOR LOOP BABY!

---
+++ 023 - Intro to Loop/index.js
@@ -0,0 +1,12 @@
+//types of loops (ICU)
+//I  - INITIALIZATION
+//C - CONDITION
+//U - UPDATE
+
+
+//while loop
+let i = 0;
+while(i < 10){
+    console.log("WHILE LOOP", i++)
+    i++;
+};
---
+++ 024 - Creating Loop/index.js
@@ -0,0 +1,45 @@
+//while loop
+//do while 
+//for loop
+
+//WHILE LOOP - ICU PATERN
+//iterator niya
+let i =0; //intialization
+
+while(i < 10){ //condition
+    //code
+    console.log(i);
+    i++ //update
+}
+
+
+//DO WHILE LOOP -IUC PATERN
+let z = 15; //initialization or iterator
+
+do{
+    console.log(z); //code
+    i++ //update
+}while(z < 10); //condition: IF ANG INIZIALIZER IS DAKO KAYSA CONDITION MO LOOP SYA UG IKA ISA! ELSE UG GAMAY ANG INITIALIZER UG DAKO ANG CONDITION MO INFINIT LOOP
+
+
+//FOR LOOP - ICU PARTERN 
+
+for(let w = 0; w < 10; w++){
+    console.log("MARK BELLO", w); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
