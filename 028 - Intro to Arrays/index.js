3e6aeeeda6f10cd467e564f54dfa004222d5e847
M4rk <89385349+m4rkbello@users.noreply.github.com>
2024-01-14 23:23:24 +0800

    ARRAY ARRAY!

---
+++ 028 - Intro to Arrays/index.js
@@ -0,0 +1,11 @@
+//ARRAY SAMPLE!
+
+let collections = [1, 2, 3, 50, 45, 67, 88, 77, 12, 9, 45, 43, 4, 56, 76];
+
+for(i = 0; i < collections.length; i++){
+    if(collections[i] === 2){
+        console.log(collections[i]);
+    }
+    // console.log(collections);
+    // console.log("DATA", collections[i]);
+}
