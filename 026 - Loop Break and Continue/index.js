361c7660b8350366a79dda3a3944a1f0f76611b8
M4rk <89385349+m4rkbello@users.noreply.github.com>
2024-01-14 19:42:54 +0800

    LOOP - GAMIT ICU PATERN!

--- 026 - Loop Break and Continue/index.js
+++ 026 - Loop Break and Continue/index.js
@@ -10,8 +10,19 @@ for(let i=0; i < content.length; i++){
 }
 
 
+//FOR LOOP BREAK - ICU PARTERN 
+let content1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//FOR LOOP GAMIT CONTINUE
+for(let i=0; i < content1.length; i++){
+    if(content1[i] === 2){
+        console.log("FOR LOOP BREAK NI", content1[i], +i);
+        continue;
+    }
+
+}
+
+
+//FOR LOOP GAMIT CONTINUE 
 let context = "PADAYON LANG DIHA KAYA NIMO LAYO NAKA KUNG UNSA KA SAUNA!";
 
 for(let i=0; i < context.length; i++){
