1e51d332a067dba66fce23d4363907bc4fcf74f0
M4rk <89385349+m4rkbello@users.noreply.github.com>
2024-01-15 22:41:55 +0800

    ARRAY WITH OBJECTS, ARRAY FOR LOOP, CROPPING ARRAY LENGTH WITH SET VALUE

---
+++ 030 - Using Arrays/index.js
@@ -0,0 +1,29 @@
+//initializer
+// let data = [24, 5, 4, 5, 23, 11, 34, function(){console.log("OBJECT OBJET LETRALS!")}, 659,];
+
+// console.log(data[7]());
+
+// data[7]();
+
+
+let elements = [];
+elements[1] = 12;
+elements[5] = 15;
+elements[7] = 20;
+// console.log(elements);
+
+//e forloop niya laktawan niya ang mga pagkulang nimo
+for(let i=0; i < elements.length; i++){
+    console.log(elements[i]);
+}
+
+
+//CROPPING ARRAYS!
+let numbers = [3,5,66,55,22,44,88,99,22,4,5,6,7,8,9,11,21,334];
+numbers.length = 3;
+console.log("ARRAY NUMBERS", numbers);
+
+
+
+
+
