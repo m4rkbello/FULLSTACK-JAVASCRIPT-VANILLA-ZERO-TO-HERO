a6962622c368479b88d5be7e6658b6f9317ca8e0
M4rk <89385349+m4rkbello@users.noreply.github.com>
2024-01-15 22:19:54 +0800

    MATRIX UNSHIFT INDEX

--- 029 - Intro to Multidimensional Array/index.js
+++ 029 - Intro to Multidimensional Array/index.js
@@ -9,7 +9,7 @@ let matrix = [
 
 // Accessing elements in the 2D array
 console.log(matrix[0][0]); // Output: 1 PINAKAUNA IS INDEX SA ARRAY NA COLLECTION [0]  IS SA UNANG BLOCK THEN [0]
-console.log(matrix[1][5]); // Output: 9
+console.log(matrix[1][3]); // Output: 9
 
 // Iterating over the elements of the 2D array
 for (let i = 0; i < matrix.length; i++) {
@@ -18,11 +18,12 @@ for (let i = 0; i < matrix.length; i++) {
     }
 }
 
-// Adding a new row to the 2D array
+// Adding a new row to the 2D array - IPASA NIYA GAMIT PUSH METHOD IPASA SA LAST
 matrix.push([10, 11, 12]);
+matrix[2].unshift([20, 30, 40, 90])
 
-// Adding a new element to an existing row
-matrix[0].push(13);
+// Adding a new element to an existing row - IPASA SA INDEX [0] O PINAKAUNA NGA COLLECTION SA ARRAYS TAPOS ANG 13 IDUGANG SA PILAST GAMIT PUSH 
+matrix[0].push(13); 
 
 // Updated matrix
 console.log(matrix);
