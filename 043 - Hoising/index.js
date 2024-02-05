8127abf5368f8489bcb38a82fa5d27c7e6de1bb4
m4rkbello <markbello10041999@gmail.com>
2024-02-05 22:05:32 +0800

    KALAHIAN SA HOISTING FUNC DECLERATION VS. FUNC EXPRESSION

---
+++ 043 - Hoising/index.js
@@ -0,0 +1,24 @@
+//HOISTING PRACTICE
+
+
+//function decleration
+function doThis(){
+    console.log("AYAW KOL!");
+}
+doThis();
+
+
+
+//function expression
+
+var ayawKol = function(){
+    console.log("BUNALI KOL!");
+}
+
+ayawKol()
+
+
+
+
+
+
